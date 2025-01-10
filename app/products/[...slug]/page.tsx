5468ffc4cfcfc8b055534b98b982048fbebfe62b
kid-us <kidushh29@gmail.com>
2025-01-10 09:39:57 +0300

    Dynamic routes

---
+++ app/products/[...slug]/page.tsx
@@ -0,0 +1,14 @@
+const ProductsPage = async ({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) => {
+  const slug = (await params).slug;
+  return (
+    <div>
+      <p>ProductsPage {slug}</p>
+    </div>
+  );
+};
+
+export default ProductsPage;
