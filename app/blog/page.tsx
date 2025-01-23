732a8ab55ef3dfcec157774b62b5070f0fffbf90
kid-us <kidushh29@gmail.com>
2025-01-24 01:12:21 +0300

    Linking pages

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,9 +1,12 @@
+import Link from "next/link";
 import React from "react";
 
 const BlogPage = () => {
   return (
     <div>
       <p>Blog Page</p>
+
+      <Link href={"blog/slug"}>Link example</Link>
     </div>
   );
 };
