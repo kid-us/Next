190e154ad9e041a40143be16535ac47444d2c5b9
kid-us <kidushh29@gmail.com>
2025-01-27 15:06:51 +0300

    Not found.

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PageNotFound = () => {
+  return (
+    <div>
+      <p>Page doesn't exist!</p>
+    </div>
+  );
+};
+
+export default PageNotFound;
--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -1,7 +1,10 @@
+import { notFound } from "next/navigation";
+
 interface Props {
   params: { id: number };
 }
 const UserDetailPage = ({ params: { id } }: Props) => {
+  if (id > 10) return notFound();
   return (
     <div>
       <p>User ID is : {id}</p>
