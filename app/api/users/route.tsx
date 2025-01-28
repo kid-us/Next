739a418fe4a647fcf630e0a27964394bdb241f03
kid-us <kidushh29@gmail.com>
2025-01-28 17:46:37 +0300

    Create new users.

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -2,14 +2,14 @@ import { NextRequest, NextResponse } from "next/server";
 import userSchema from "./schema";
 import { prisma } from "@/prisma/client";
 
-// GET
+// GET User
 export const GET = async (request: NextRequest) => {
   const users = await prisma.user.findMany();
   return NextResponse.json(users);
 };
 
-// POST
-export async function POST(request: NextRequest) {
+// Create new user
+export const POST = async (request: NextRequest) => {
   const body = await request.json();
 
   const validation = userSchema.safeParse(body);
@@ -17,11 +17,26 @@ export async function POST(request: NextRequest) {
   if (validation.error)
     return NextResponse.json(validation.error.errors, { status: 400 });
 
-  return NextResponse.json(
-    {
-      id: 1,
-      name: body.name,
+  // Check email
+  const user = await prisma.user.findUnique({
+    where: {
+      email: body.email,
     },
-    { status: 200 }
-  );
-}
+  });
+
+  if (user)
+    return NextResponse.json(
+      { error: "Email address already exist" },
+      { status: 400 }
+    );
+
+  // Create
+  const newUser = await prisma.user.create({
+    data: {
+      name: body.name,
+      email: body.email,
+    },
+  });
+
+  return NextResponse.json(newUser, { status: 200 });
+};
