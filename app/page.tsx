cba9f6001a288b4f7e6b36ba4fb4572657cdeb12
kid-us <kidushh29@gmail.com>
2025-01-29 18:10:28 +0300

    Getting session from server component.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
 import Link from "next/link";
 import ProductCard from "./components/ProductCard";
+import { authOptions } from "./api/auth/[...nextauth]/route";
+import { getServerSession } from "next-auth";
 
-const Home = () => {
+const Home = async () => {
+  const session = await getServerSession(authOptions);
   return (
     <div>
-      <p>Hello Next-JS</p>
+      <p>
+        Hello{" "}
+        {session ? <span>{session.user!.name}</span> : <span>Next-JS</span>}
+      </p>
       <Link href={"/users"} className="text-blue-500 underline">
         Users
       </Link>
